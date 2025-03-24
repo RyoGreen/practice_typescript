4d0547a53ce68e8dd58d5223b478df4ba533e58f
RyoGreen <41415120ryo@gmail.com>
2025-03-24 17:40:10 +0900

    fix

--- typescript/index.js
+++ typescript/index.js
@@ -1,7 +1,36 @@
-var hello = "hello";
 var world = "world";
 console.log(hello + world);
+console.log(hello.length);
 function increment(num) {
     return num + 1;
 }
 console.log(increment(2));
+var BankAccount = /** @class */ (function () {
+    function BankAccount(initialBalance, accountNumber) {
+        this.balance = initialBalance;
+        this.accountNumber = accountNumber;
+    }
+    BankAccount.prototype.deposit = function (amount) {
+        this.balance += amount;
+    };
+    BankAccount.prototype.getBalance = function () {
+        return this.balance;
+    };
+    return BankAccount;
+}());
+var product = {
+    name: " aa",
+    price: 100,
+};
+console.log(product.name);
+console.log(product.price);
+var increment2 = function (num) { return num + 1; };
+function hello(person) {
+    if (typeof person === "string") {
+        console.log("Hello ".concat(person));
+    }
+}
+function test() {
+    console.log("test");
+}
+test();
--- typescript/index.ts
+++ typescript/index.ts
@@ -1,9 +1,51 @@
-const hello = "hello";
 const world = "world";
 console.log(hello + world);
+console.log(hello.length);
 
 function increment(num: number) {
   return num + 1;
 }
 
 console.log(increment(2));
+
+class BankAccount {
+  private balance: number;
+  public accountNumber: number;
+
+  constructor(initialBalance: number, accountNumber: number) {
+    this.balance = initialBalance;
+    this.accountNumber = accountNumber;
+  }
+
+  public deposit(amount: number) {
+    this.balance += amount;
+  }
+
+  protected getBalance(): number {
+    return this.balance;
+  }
+}
+
+const product = {
+  name: " aa",
+  price: 100,
+};
+
+console.log(product.name);
+console.log(product.price);
+
+const increment2 = (num: number) => num + 1;
+
+function hello(person: string): void;
+function hello(persons: string[]): void;
+function hello(person: string | string[]): void {
+  if (typeof person === "string") {
+    console.log(`Hello ${person}`);
+  }
+}
+
+function test(): void {
+  console.log("test");
+}
+
+test();
