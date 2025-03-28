067ee0750f87df4cdc655ea43fa16f0780ccba94
RyoGreen <41415120ryo@gmail.com>
2025-03-28 23:41:23 +0900

    delete unnecesarry code

--- typescript/index.ts
+++ typescript/index.ts
@@ -1,13 +1,3 @@
-const world = "world";
-console.log(hello + world);
-console.log(hello.length);
-
-function increment(num: number) {
-  return num + 1;
-}
-
-console.log(increment(2));
-
 class BankAccount {
   private balance: number;
   public accountNumber: number;
@@ -25,27 +15,3 @@ class BankAccount {
     return this.balance;
   }
 }
-
-const product = {
-  name: " aa",
-  price: 100,
-};
-
-console.log(product.name);
-console.log(product.price);
-
-const increment2 = (num: number) => num + 1;
-
-function hello(person: string): void;
-function hello(persons: string[]): void;
-function hello(person: string | string[]): void {
-  if (typeof person === "string") {
-    console.log(`Hello ${person}`);
-  }
-}
-
-function test(): void {
-  console.log("test");
-}
-
-test();
