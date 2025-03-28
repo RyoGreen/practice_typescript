7c824683311d8d901708e5bf11221d001bf7f6d4
RyoGreen <41415120ryo@gmail.com>
2025-03-29 00:01:13 +0900

    enum

--- typescript/index.ts
+++ typescript/index.ts
@@ -1,17 +1,33 @@
 class BankAccount {
   private balance: number;
   public accountNumber: number;
+  public accountType: AccountType;
 
-  constructor(initialBalance: number, accountNumber: number) {
+  constructor(initialBalance: number, accountNumber: number, accountType: AccountType) {
     this.balance = initialBalance;
     this.accountNumber = accountNumber;
+    this.accountType = accountType;
   }
 
   public deposit(amount: number) {
+    this.getBalance();
     this.balance += amount;
   }
 
-  protected getBalance(): number {
+  private getBalance(): number {
     return this.balance;
   }
+
+  public withdraw(amount: number) {
+    if (this.balance < amount) {
+      throw new Error("Insufficient funds");
+    }
+    this.balance -= amount;
+  }
+}
+
+enum AccountType {
+  Individual = 1,
+  Company = 2,
+  Other = 99,
 }
