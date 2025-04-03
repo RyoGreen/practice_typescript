a177ae42f2a8e42fa0a16820514f032d0a5d0a38
RyoGreen <41415120ryo@gmail.com>
2025-04-03 22:45:03 +0900

    new user controller

--- nestjs/src/app.controller.ts
+++ nestjs/src/app.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get } from '@nestjs/common';
+import { Body, Controller, Get, Post } from '@nestjs/common';
 import { AppService } from './app.service';
 
 @Controller()
@@ -8,5 +8,27 @@ export class AppController {
   @Get()
   getHello(): string {
     return this.appService.getHello();
+  }
+  @Get('goodbye')
+  getGoodbye(): string {
+    return this.appService.getGoodbye();
+  }
+  @Post('post')
+  postBody(@Body() createUserDto: { name: string; age: number }): string {
+    return `Received name: ${createUserDto.name}, age: ${createUserDto.age}`;
+  }
+}
+
+@Controller('users')
+export class UserController {
+  constructor(private readonly appService: AppService) {}
+
+  @Get()
+  getUsers(): string {
+    return this.appService.getHello();
+  }
+  @Get('goodbye')
+  getGoodbye(): string {
+    return this.appService.getGoodbye();
   }
 }
--- nestjs/src/app.service.ts
+++ nestjs/src/app.service.ts
@@ -5,4 +5,12 @@ export class AppService {
   getHello(): string {
     return 'Hello World!';
   }
+
+  getGoodbye(): string {
+    return 'good bye';
+  }
+
+  postHello(): string {
+    return 'Hello World!';
+  }
 }
