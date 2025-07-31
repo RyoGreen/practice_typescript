cf2e678f9b91317c7031dbd70eb71734e998f028
野原崚 <nohararyo@nohararyonoMacBook-Pro.local>
2025-07-31 22:58:18 +0900

    create infra

---
+++ nestjs/src/infrastructure/user.ts
@@ -0,0 +1,37 @@
+import { IUserRepository, User } from "src/domain/user";
+import { prisma } from "./postgresql";
+import { emit } from "process";
+
+export class UserRepository implements IUserRepository {
+    async Get(): Promise<User[]> {
+        const users = await prisma.user.findMany();
+        return users.map(user => new User({
+            id: user.id,
+            email: user.email,
+        }))
+    }
+    async GetById(id: number): Promise<User | null> {
+        const user = await prisma.user.findUnique({
+            where: {
+                id: id,
+            }
+        });
+        if (!user) return null;
+        return new User({
+            id: user.id,
+            email: user.email,
+            name: user.name,
+        }
+        );
+
+    }
+    async Create(post: User): Promise<User> {
+
+    }
+    async Update(post: User): Promise<User> {
+
+    }
+    async Delete(id: number): Promise<void> {
+
+    }
+}
