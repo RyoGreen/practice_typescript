34e5b60a8bd17b2af4b73ee4c10eeafa94df2210
RyoGreen <41415120ryo@gmail.com>
2025-08-03 00:25:57 +0900

    prisma format

--- nestjs/src/app.controller.ts
+++ nestjs/src/app.controller.ts
@@ -5,34 +5,32 @@ import { User } from '@prisma/client';
 
 @Controller()
 export class AppController {
-    constructor(private readonly appService: AppService) { }
+  constructor(private readonly appService: AppService) {}
 
-    @Get()
-    getHello(): string {
-        return this.appService.getHello();
-    }
-    @Get('goodbye')
-    getGoodbye(): string {
-        return this.appService.getGoodbye();
-    }
-    @Post('post')
-    postBody(@Body() createUserDto: { name: string; age: number }): string {
-        return `Received name: ${createUserDto.name}, age: ${createUserDto.age}`;
-    }
+  @Get()
+  getHello(): string {
+    return this.appService.getHello();
+  }
+  @Get('goodbye')
+  getGoodbye(): string {
+    return this.appService.getGoodbye();
+  }
+  @Post('post')
+  postBody(@Body() createUserDto: { name: string; age: number }): string {
+    return `Received name: ${createUserDto.name}, age: ${createUserDto.age}`;
+  }
 }
 
 @Controller('users')
 export class UserController {
-    constructor(private readonly userUsecase: UserUsecase) { }
+  constructor(private readonly userUsecase: UserUsecase) {}
 
-    @Get()
-    getUsers(): Promise<User[]> {
-        return this.userUsecase.List();
-    }
-    @Get('/:id')
-    getUser(@Param() id: number): Promise<User | null> {
-        return this.userUsecase.Get(id);
-    }
+  @Get()
+  getUsers(): Promise<User[]> {
+    return this.userUsecase.List();
+  }
+  @Get('/:id')
+  getUser(@Param() id: number): Promise<User | null> {
+    return this.userUsecase.Get(id);
+  }
 }
-
-
