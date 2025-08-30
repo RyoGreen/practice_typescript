82d4387e01c96bac34a664d3b38e99792cd947fb
RyoGreen <41415120ryo@gmail.com>
2025-08-30 15:42:28 +0900

    reset

--- react/src/App.tsx
+++ react/src/App.tsx
@@ -1,47 +1,20 @@
-import type { StudentFormData } from "./api/student";
 import "./App.css";
-import { useForm, FormProvider } from "react-hook-form";
-import BasicInfo from "./components/BasicInfo";
-import { postStudent } from "./api/student";
 
 function App() {
-    const methods = useForm<StudentFormData>({
-        defaultValues: {
-            firstName: "",
-            lastName: "",
-            email: "",
-            schoolId: "",
-            departmentId: ""
-        }
-    });
-
-    const onSubmit = (data: StudentFormData) => {
-        postStudent(data)
-            .then(response => {
-                console.log("送信成功:", response);
-            })
-            .catch(error => {
-                console.error("送信失敗:", error);
-            });
-    };
-
     return (
-        <FormProvider {...methods}>
-            <form onSubmit={methods.handleSubmit(onSubmit)}>
-                <BasicInfo schools={
-                    [
-                        { id: "1", name: "学校A" },
-                        { id: "2", name: "学校B" }
-                    ]
-                } departments={
-                    [
-                        { id: "1", name: "学科A" },
-                        { id: "2", name: "学科B" }
-                    ]
-                }
-                />
-                <button type="submit">送信</button>
-            </form>
-        </FormProvider>);
+        <div className="App">
+            <h1>Hello Vite + React!</h1>
+            <a href="https://vitejs.dev" target="_blank" rel="noreferrer">
+                <button>Vite Docs</button>
+            </a>
+            <a
+                href="https://reactjs.org"
+                target="_blank"
+                rel="noreferrer"
+            >
+                <button>React Docs</button>
+            </a>
+        </div>
+    );
 }
 export default App
