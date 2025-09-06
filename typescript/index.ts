95a3e6e4a13029539593f4d1950d873f9a1e7e87
RyoGreen <41415120ryo@gmail.com>
2025-09-07 00:18:51 +0900

    index

--- typescript/index.ts
+++ typescript/index.ts
@@ -1,7 +1,9 @@
 import { tryCatch } from "./trycatch";
 import { loop } from "./loop"
+import { main } from "./functional";
 
 tryCatch();
 console.log("--------------------------------------------------");
 loop();
-
+console.log("--------------------------------------------------");
+main();
