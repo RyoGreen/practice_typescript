ab05dbe0aec88324797486403a147dc9af83cf6e
RyoGreen <41415120ryo@gmail.com>
2025-09-13 16:10:16 +0900

    change function

--- react/src/pages/Form.tsx
+++ react/src/pages/Form.tsx
@@ -1,10 +1,16 @@
-const Form: React.FC = () => {
+export default function Form() {
     return (
-        <div>
-            Form Page
-        </div>
-
+        <form>
+            <div>
+                <input type="text" placeholder="Enter text" />
+            </div>
+            <div>
+                <input type="number" placeholder="Enter number" />
+            </div>
+            <div>
+                <input type="email" placeholder="Enter email" />
+            </div>
+            <button type="submit">Submit</button>
+        </form>
     )
 }
-
-export default Form;
