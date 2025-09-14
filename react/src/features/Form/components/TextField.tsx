22ba72458a7fe2cfbc48ea5d4eade5ae1ff835c6
RyoGreen <41415120ryo@gmail.com>
2025-09-15 00:32:47 +0900

    error output

--- react/src/features/Form/components/TextField.tsx
+++ react/src/features/Form/components/TextField.tsx
@@ -1,11 +1,13 @@
 import type { UseFormRegister } from "react-hook-form";
 import type { UserFormData } from "../../../schemas/user";
+import type { FieldErrors } from "react-hook-form";
 
 type Props = {
     register: UseFormRegister<UserFormData>;
+    errors: FieldErrors<UserFormData>;
 };
 
-export default function TextField({ register }: Props) {
+export default function TextField({ register, errors }: Props) {
     return (
         <div className="space-y-2">
             <input
@@ -13,13 +15,21 @@ export default function TextField({ register }: Props) {
                 placeholder="First name"
                 {...register("firstName")}
                 className="border px-2 py-1 rounded w-full"
+
             />
+            {errors.firstName && (
+                <p className="text-red-500 text-sm">{errors.firstName.message}</p>
+            )}
+
             <input
                 type="text"
                 placeholder="Last name"
                 {...register("lastName")}
                 className="border px-2 py-1 rounded w-full"
             />
+            {errors.lastName && (
+                <p className="text-red-500 text-sm">{errors.lastName.message}</p>
+            )}
         </div>
     );
 }
--- react/src/pages/Form.tsx
+++ react/src/pages/Form.tsx
@@ -9,7 +9,7 @@ import type { UserFormData } from "../schemas/user";
 
 const Form: React.FC = () => {
     const user = useContext(UserContext)
-    const { register, handleSubmit, } = useForm<UserFormData>({
+    const { register, handleSubmit, formState: { errors } } = useForm<UserFormData>({
         defaultValues: {
             firstName: user ? user.firstName : "first_name",
             lastName: user ? user.lastName : "last_name",
@@ -21,7 +21,7 @@ const Form: React.FC = () => {
     return (
         <div className="min-h-full flex flex-col items-center justify-center bg-gray-100 p-4 space-y-4">
             <form onSubmit={onSubmit} className="bg-white p-6 rounded shadow-md space-y-4 w-full max-w-sm">
-                <TextField register={register} />
+                <TextField register={register} errors={errors} />
                 <NumberField register={register("number")} />
                 <EmailField register={register("email")} />
                 <button type="submit" className="w-full bg-blue-500 text-white py-2 px-4 rounded hover:bg-blue-600 transition duration-200">
